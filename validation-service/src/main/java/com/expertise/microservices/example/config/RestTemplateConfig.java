06d0943e958c8c73eca06b1033aebaa3932622ae
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-06-28 00:20:59 +0530

    feat: implemented RestTemplate configuration for inter-service communication

--- validation-service/src/main/java/com/expertise/microservices/example/config/RestTemplateConfig.java
+++ validation-service/src/main/java/com/expertise/microservices/example/config/RestTemplateConfig.java
@@ -1,8 +1,14 @@
 package com.expertise.microservices.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
