4a414f38973c9693e3c4e02fef0d29da4e2e2bde
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-06-29 14:35:20 +0530

    feat: added UserController with REST CRUD endpoints

--- storage-service/src/main/java/com/expertise/microservices/example/controller/UserController.java
+++ storage-service/src/main/java/com/expertise/microservices/example/controller/UserController.java
@@ -1,13 +1,37 @@
 package com.expertise.microservices.example.controller;
 
+import com.expertise.microservices.example.model.User;
 import com.expertise.microservices.example.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/store")
 public class UserController {
 
     @Autowired
     private UserServiceI userServiceI;
+
+    @PostMapping
+    public ResponseEntity<User> storeUser(@RequestBody User user) {
+        return ResponseEntity.ok(userServiceI.saveUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userServiceI.getUserByID(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return ResponseEntity.ok(userServiceI.updateUser(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userServiceI.deleteUser(id);
+        return ResponseEntity.ok("Deleted successfully");
+    }
 
 }
--- storage-service/src/main/java/com/expertise/microservices/example/service/UserServiceI.java
+++ storage-service/src/main/java/com/expertise/microservices/example/service/UserServiceI.java
@@ -1,4 +1,15 @@
 package com.expertise.microservices.example.service;
 
+import com.expertise.microservices.example.model.User;
+
 public interface UserServiceI {
+
+    User saveUser(User user);
+
+    User getUserByID(Long id);
+
+    User updateUser(Long id, User user);
+
+    void deleteUser(Long id);
+
 }
