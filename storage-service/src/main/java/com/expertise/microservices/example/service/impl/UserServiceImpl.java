58e53bbbfafeba1b18458790653d3221ece6728c
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-06-29 14:38:09 +0530

    feat: implemented repository and service layers for user CRUD

--- storage-service/src/main/java/com/expertise/microservices/example/service/impl/UserServiceImpl.java
+++ storage-service/src/main/java/com/expertise/microservices/example/service/impl/UserServiceImpl.java
@@ -1,14 +1,37 @@
 package com.expertise.microservices.example.service.impl;
 
+import com.expertise.microservices.example.model.User;
 import com.expertise.microservices.example.repository.UserRepository;
 import com.expertise.microservices.example.service.UserServiceI;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserServiceI {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserByID(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with ID: " + id));
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 
 }
